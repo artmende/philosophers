162df9d1f7781cae308f4877ec630b7133093fbd
artmende <artmende@student.s19.be>
2021-12-16 17:49:08 +0100

    atoi is perfect now

--- from_libft/ft_atoi.c
+++ from_libft/ft_atoi.c
@@ -6,15 +6,11 @@
 /*   By: artmende <artmende@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 17:20:56 by artmende          #+#    #+#             */
-/*   Updated: 2021/12/13 17:58:33 by artmende         ###   ########.fr       */
+/*   Updated: 2021/12/16 17:48:42 by artmende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo.h"
-
-/*
-	ft_atoi has been modified a little bit to look for int overflows.
-*/
 
 int	ft_atoi(const char *str)
 {
@@ -24,32 +20,23 @@ int	ft_atoi(const char *str)
 
 	result_l = 0;
 	minus = 1;
-	while (*str == ' ' || (*str >= 9 && *str <= 13))
+	while (str && (*str == ' ' || (*str >= 9 && *str <= 13)))
 		str++;
-	if (*str == '-' || *str == '+')
-	{
-		if (*str == '-')
-			minus = -1;
+	(void)(str && (*str == '-' || *str == '+') && str++ && *(str - 1) == '-'
+		&& (minus = -1));
+	while (str && *str == '0')
 		str++;
-	}
 	i = 0;
-	while (str[i] >= '0' && str[i] <= '9')
+	while (str && str[i] >= '0' && str[i] <= '9')
 	{
 		result_l = 10 * result_l + str[i] - '0';
 		i++;
 	}
-	if (i > 10 || (i == 10 && *str > '2')
-		|| (minus == 1 && result_l > 2147483647)
-		|| (minus == -1 && result_l > 2147483648))
-		return (0);
+	if (i > 19 || result_l < 0)
+	{
+		if (minus == -1)
+			return (0);
+		return (-1);
+	}
 	return ((int)(minus * result_l));
-}
-
-int	main(int argc, char **argv)
-{
-
-
-//	printf("%d\n", ft_atoi(argv[1]));
-//	printf("%d\n", atoi(argv[1]));
-
 }
